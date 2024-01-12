f2a85d5189d5aebd93b3a14da891aa906821df56
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-12 13:43:59 +0300

    Update TS_CaptchaSUEMemRefresh.java

--- src/main/java/com/tugalsan/api/captcha/server/TS_CaptchaSUEMemRefresh.java
+++ src/main/java/com/tugalsan/api/captcha/server/TS_CaptchaSUEMemRefresh.java
@@ -3,9 +3,12 @@ package com.tugalsan.api.captcha.server;
 import com.tugalsan.api.captcha.client.TGS_CaptchaUtils;
 import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.servlet.url.server.TS_SURLExecutor;
-import com.tugalsan.api.servlet.url.server.TS_SURLHelper;
-import com.tugalsan.api.unsafe.client.*;
+import com.tugalsan.api.servlet.url.server.handler.TS_SURLHandler;
+import com.tugalsan.api.servlet.url.server.handler.TS_SURLHandler02ForFilePng;
 import com.tugalsan.api.validator.client.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class TS_CaptchaSUEMemRefresh extends TS_SURLExecutor {
 
@@ -17,21 +20,20 @@ public class TS_CaptchaSUEMemRefresh extends TS_SURLExecutor {
     }
 
     @Override
-    public void run(TS_SURLHelper h) {
-        TGS_UnSafe.run(() -> {
-            var c = new TS_Captcha.Builder().buildPreffered(
-                    h.getParameterInteger("bg", false),
-                    h.getParameterInteger("gimp", false),
-                    h.getParameterInteger("border", false),
-                    h.getParameterInteger("txt", false),
-                    h.getParameterInteger("word", false),
-                    h.getParameterInteger("noise", false),
-                    onlyNumbers == null ? false : onlyNumbers.validate(h)
-            );
-            TS_CaptchaMemUtils.setServer(h.rq, c.getAnswer());
-            h.addHeaderNoCache().compileForPng().transferPng(c.getImage());
-        }, e -> d.ce("run", e.getMessage()));
+    public void run(HttpServlet servlet, HttpServletRequest rq, HttpServletResponse rs) {
+        var png = TS_SURLHandler.of(servlet, rq, rs).permitNoCache().png();
+        var c = new TS_Captcha.Builder().buildPreffered(
+                png.getParameterInteger("bg", false),
+                png.getParameterInteger("gimp", false),
+                png.getParameterInteger("border", false),
+                png.getParameterInteger("txt", false),
+                png.getParameterInteger("word", false),
+                png.getParameterInteger("noise", false),
+                onlyNumbers == null ? false : onlyNumbers.validate(png)
+        );
+        TS_CaptchaMemUtils.setServer(rq, c.getAnswer());
+        png.transferPng(c.getImage());
     }
 
-    public static TGS_ValidatorType1<TS_SURLHelper> onlyNumbers;
+    public static TGS_ValidatorType1<TS_SURLHandler02ForFilePng> onlyNumbers;
 }

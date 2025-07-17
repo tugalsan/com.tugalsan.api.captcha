56a029b31fce907c9ec71b626aecafa21a6098e2
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-07-17 15:26:21 +0300

    .

--- src/main/java/com/tugalsan/api/gwt/captcha/server/TS_CaptchaClientValues.java
+++
@@ -1,7 +0,0 @@
-package com.tugalsan.api.gwt.captcha.server;
-
-import com.tugalsan.api.union.client.TGS_UnionExcuse;
-
-public record TS_CaptchaClientValues(TGS_UnionExcuse<String> clientIp, CharSequence guess) {
-
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,5 @@
-module com.tugalsan.api.gwt.captcha {
-    requires java.desktop;
+module com.tugalsan.api.captcha {
     requires gwt.user;
-    requires com.tugalsan.api.thread;
-    requires com.tugalsan.api.log;
-    
-    requires com.tugalsan.api.union;
-    requires com.tugalsan.api.function;
-    requires com.tugalsan.api.list;
-    requires com.tugalsan.api.network;
-    requires com.tugalsan.api.time;
-    requires com.tugalsan.api.url;
-    
-    requires com.tugalsan.api.random;
-    requires com.tugalsan.api.servlet.url;
-    exports com.tugalsan.api.gwt.captcha.client;
-    exports com.tugalsan.api.gwt.captcha.server;
+    exports com.tugalsan.api.captcha.client;
+    exports com.tugalsan.api.captcha.server;
 }
